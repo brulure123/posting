781d3d28dd8eecb99f608341eb4701e504ffe967
brulure123 <benraj.200@gmail.com>
2025-03-10 15:07:17 +0200

    Setting rooting and add somme components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {CategoryComponent} from './components/category/category.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule)
+  }
+];
